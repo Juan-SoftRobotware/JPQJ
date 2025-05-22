e75093543dbe097ebbb2f822b20e6444862b3159
Juan-SoftRobotware <jp.quinones2009@gmail.com>
2025-05-22 17:50:47 +0000

    added 'the' to both strings

--- RandomPythonFile.py
+++ RandomPythonFile.py
@@ -1,8 +1,8 @@
 def sum(b, c):
     return (b + c)
 
-b = int(input('Enter 1st number: '))
-c = int(input('Enter 2nd number: '))
+b = int(input('Enter the 1st number: '))
+c = int(input('Enter the 2nd number: '))
 
 print(f'Sum of {b} and {c} is {sum(b, c)}')
 "There is in fact something to see here lil bro"
